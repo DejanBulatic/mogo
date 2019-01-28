a1d104dc3a1ec8341ab96ecb22322ca4a51e133b
Dejan <Dejan@DESKTOP-F78QALM>
2019-01-28 10:57:45 +0100

    animated

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,43 @@
+$(document).ready(function () {
 
-function onclickMenu() {
-    document.getElementById('headMenu').classList.toggle('change');
 
-    document.getElementById('custom-top-header').classList.toggle('headerbg');
-}
+    $('#headMenu').click(function () {
+        $('#headMenu').toggleClass('change');
+        $('#custom-top-header').toggleClass('headerbg');
+    });
 
-$(window).on('scroll', function() {
-			if($(window).scrollTop()) {
-				$('header').addClass('custom-head-scrol');
-			}
-			else {
-				$('header').removeClass('custom-head-scrol');
-			}
-		})
-	
-	  
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop()) {
+            $('header').addClass('custom-head-scrol');
+        } else {
+            $('header').removeClass('custom-head-scrol');
+        }
+    });
 
+    function animation(){
+        var windowHeight = $(window).height();
+        var scrollTop = $(window).scrollTop();
+        
+        $('.animation').each(function(){
+            var topPosition = $(this).offset().top;
+            if(topPosition < scrollTop + windowHeight - 100){
+                var animation = $(this).data('animation');
+                var delay = $(this).data('delay');
+                delay = parseInt(delay);
+                $(this).css('animation-delay', delay+'s').addClass(animation);
+            }
+            
+        });
+    }
+    
+    
+    animation();
+    
+    $(window).scroll(function(){
+        animation();
+    });
+    
+    
+
+
+});
